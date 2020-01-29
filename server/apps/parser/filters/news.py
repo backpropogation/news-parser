b31ccbf3c1adc74939c7a949c3204f6f82c4ebb9
Maxim <jack.moriarty@mail.ru>
2020-01-29 14:17:05 +0700

    Fix filtering against site's url, reove throttling

--- server/apps/parser/filters/news.py
+++ server/apps/parser/filters/news.py
@@ -1,13 +1,13 @@
 from django_filters import rest_framework as filters
 
-from apps.parser.models import News, Site
+from apps.parser.models import News
 
 
 class NewsFilter(filters.FilterSet):
     title = filters.CharFilter(field_name="title", lookup_expr='icontains')
     description = filters.CharFilter(field_name="description", lookup_expr='icontains')
     sub_title = filters.CharFilter(field_name="sub_title", lookup_expr='icontains')
-    site = filters.ModelChoiceFilter(field_name="site", to_field_name='url', queryset=Site.objects.all())
+    site = filters.CharFilter(field_name="site__url", lookup_expr='icontains')
 
     class Meta:
         model = News
